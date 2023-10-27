72e95a61e962f1ab57f842d934a4565ee209bd41
trevorgildersleeve <trevor@cloudbasesoftware.com>
2023-10-27 15:00:41 -0400

    Commit 2

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            CDK, Amplify, & Next.js - Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
